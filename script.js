e34e9a280a8808addab77c8f704a8451e2f92fff
Sathwik-Perkari <167493772+Sathwik-Perkari@users.noreply.github.com>
2024-04-19 13:52:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+/*============= toggle icon menu =============*/
+let subMenu = document.getElementById("subMenu");
+            
+                menu.onclick = () => {
+                menu.classList.toggle('bx-x');
+                subMenu.classList.toggle("open-menu");
+                };
+
+// animation footer on scroll
+let footer = document.querySelector("footer");
+             footer.onmouseover = () => {
+footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+                };
+
+/*============= typed js =============*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Designer', 'Packaging', 'Logos', 'Broucher', 'Website', 'Brand', 'Nagasai'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
